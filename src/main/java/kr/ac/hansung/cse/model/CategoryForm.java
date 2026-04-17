b8b0f6692fb261017db7739a77865a8136ffcb7b
suc0819 <suc0819@gmail.com>
2026-04-17 18:23:10 +0900

    카테고리 관리 화면

---
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -0,0 +1,30 @@
+package kr.ac.hansung.cse.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CategoryForm {
+    @NotBlank(message = "카테고리 이름을 입력하세요")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
+
+    private long id;
+    private String name;
+
+    public Category toEntity() {
+        return new Category(this.name);
+    }
+
+    public static CategoryForm from(Category category) {
+        CategoryForm form = new CategoryForm();
+        form.id = category.getId();
+        form.name = category.getName();
+        return form;
+    }
+
+}
